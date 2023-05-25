e5a4f8449183dfc23d16016d07340d52ca64820b
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-05-25 14:34:11 +0700

    sws

--- js/app.js
+++ js/app.js
@@ -19,6 +19,22 @@ $(document).ready(function () {
     AOS.init()
 })
 
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 1500, 2023, 4500);
+
 $(".category-airline").click(function () {
     var id = $(this).attr("id");
     var toShow = "#show-" + id;
