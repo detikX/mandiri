8157c5b2ecb817fbb4ad5944e9688cb9f6860806
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-05-25 12:32:15 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -19,71 +19,54 @@ $(document).ready(function () {
     AOS.init()
 })
 
-//gsap
-// gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".box",
-//         start: "center center",
-//         end: "bottom top",
-//         scrub: 1,
-//         pin: true
-//     }
-// })
-//     .from(".box", {
-//         opacity: 0
-//     })
-//     .from(".text1", {
-//         x: innerWidth * 1
-//     })
-//     .from(".text2", {
-//         x: innerWidth * -1
-//     })
-//     .from(".text3", {
-//         x: innerWidth * 1
-//     })
-//     .from(".logo", {
-//         y: innerHeight * 1,
-//         rotate: 360
-//     })
-
-
-// gsap.timeline({
-//         scrollTrigger: {
-//             trigger: ".box2",
-//             start: "top top",
-//             end: "bottom top",
-//             scrub: 1,
-//             pin: true
-//         }
-//     })
-//     .from(".box2", {
-//         opacity: 0,
-//     })
-//     .from(".text4", {
-//         y: innerHeight * 1,
-//         stagger: {
-//             amount: 0.5,
-//         }
-//     })
-
-// $(".helping-hidden").css({
-//     'visibility': 'hidden',
-//     'height': '0px',
-// // });
-$(".helping-hidden").hide()
-$("#mulai").click(function () {
-    // $(".helping-hidden").css({
-    //     'visibility': 'visible',
-    //     'height': 'auto',
-    // });
-    // new WOW().init();
-    // AOS.init()
-    $(".helping-hidden").show();
-    $('html, body').animate({
-        scrollTop: $(".helping-hidden").offset().top - 20
-    }, 1000);
-
-    // $()
-    AOS.refresh();
-    $(this).hide();
-})
+$(".category-airline").click(function () {
+    var id = $(this).attr("id");
+    var toShow = "#show-" + id;
+    console.log(toShow);
+    $(".airline-content").not(toShow).hide();
+    $(toShow).fadeIn("slow");
+    $(".twox").show();
+  
+    if ($(".detail-refund").children().hasClass("open")){
+      $(toShow).removeClass("open").addClass("close-info").slideDown(function(){
+        $('html, body').animate({
+              scrollTop: $(".refund").offset().top + 20
+          });
+      });
+      console.log("harus fade")
+    }
+  
+    if ($(".detail-refund").children().hasClass("close-info")) {
+      $(toShow)
+        .removeClass("close-info")
+        .addClass("open")
+        .slideDown(function () {
+          $("html, body").animate({
+            scrollTop: $(".refund").offset().top + 20,
+          });
+        });
+      console.log("harus slide");
+    }
+  
+    $('.detail-refund ul li.category-detail').removeClass("activeMenu");
+    $('.detail-refund ul li.category-detail:first-child').addClass("activeMenu");
+    // $('.wrapper-box').css("display","block");
+    $('.wrapper-box').css("display","none");
+    $(toShow).children('.wrapper-box').first().css("display","block")
+  });
+  
+$(".closer").click(() => {
+    $(".airline-content").removeClass("open").addClass("close-info");
+    $(".twox").fadeOut();
+    $(".airline-name").removeClass("bg-category");
+    $(".airline-content") /*.fadeOut()*/
+      .fadeOut();
+      $("html, body").animate({
+        scrollTop: $(".contentx").offset().top + 20,
+      })
+  });
+  
+  $(".category-click .airline-name").click(function () {
+    $(".category-click .airline-name").removeClass("bg-category");
+    $(this).addClass("bg-category");
+  });
