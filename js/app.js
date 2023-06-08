5367182b84c2ad93e7e1e00ec948c239eb36ac23
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-06-08 11:41:35 +0700

    f

--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ $(".closer").click(() => {
     $(".airline-content") /*.fadeOut()*/
         .fadeOut();
     $("html, body").animate({
-        scrollTop: $(".contentx").offset().top + 20,
+        scrollTop: $(".contentx").offset().top - 60,
     })
 });
 
